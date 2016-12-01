1d22c9a135446107d7be4bdbc00ae8c1b7231da6
Jake VandeWalle <jakevandewalle@gmail.com>
2016-12-01 17:41:12 -0600

    Cleaned it up

--- TipUpSlave.py
+++ TipUpSlave.py
@@ -3,34 +3,21 @@ import CHIP_IO.GPIO as GPIO
 import time
 
 GPIO.cleanup()
-GPIO.setup("XIO-P0", GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup("XIO-P0", GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 GPIO.setup("XIO-P2", GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
 
 client = paho.Client()
 client.connect("172.20.0.1", 1883)
-#client.on_connect = on_connect
-#client.on_publish = on_publish
-#client.on_disconnect = on_disconnect
 
-#client.connect("172.20.0.1", 1883)
-
-#def on_connect(mosq, obj, rc): 
 print ("CONNECTED")
 while True:
   if (GPIO.input("XIO-P0") == False):
-    print ("Shit this button actually works..")
-    time.sleep(1)
+    print ("Button P0 Pressed")
     client.publish('tipup', 'flag')
+    time.sleep(1)
 
   if (GPIO.input("XIO-P2") == False):
-    print ("And this one does too motherfucker")
-    time.sleep(1)
+    print ("Button P2 Pressed")
     client.publish('tipup', 'off')
+    time.sleep(1)
 
-
-#def on_publish(mosq, obj, mid):
- # print ("JUST PUBLISHED")
-
-
-#client.connect("172.20.0.1", 1883)
