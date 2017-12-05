195da1411dc460782ef1a2f2be7ce660482bcf31
Jake VandeWalle <jakevandewalle@gmail.com>
2017-12-05 15:37:20 -0600

    Update TipUpSlave.py

--- TipUpSlave.py
+++ TipUpSlave.py
@@ -1,54 +1,56 @@
-#import paho.mqtt.client as paho
-#import CHIP_IO.GPIO as GPIO
-#import time
-
-#GPIO.cleanup()
-#GPIO.setup("XIO-P0", GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-#GPIO.setup("XIO-P2", GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-#client = paho.Client()
-#client.connect("172.20.0.1", 1883)
-
-#print ("CONNECTED")
-
-#client.loop_start()
-
-#while True:
-  #if (GPIO.input("XIO-P0") == False):
-    ##print ("Button P0 Pressed")
-    #client.publish('tipup', 'flag')
-    #time.sleep(1)
-
-  #if (GPIO.input("XIO-P2") == False):
-    #print ("Button P2 Pressed")
-    #client.publish('tipup', 'off')
-    #time.sleep(1)
-
 import paho.mqtt.client as paho
 import CHIP_IO.GPIO as GPIO
 import time
 
+sensor = "XIO-P0"
+stealthSwitch = "XIO-P2"
+flagLight = "XIO-P4"
+
+stealth = False
+flag-down = False
+
 GPIO.cleanup()
-GPIO.setup("XIO-P0", GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup("XIO-P2", GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(sensor, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(stealthSwitch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(flagLight, GPIO.OUT)
+GPIO.output(flagLight, GPIO.HIGH)
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code " + str(rc))
+  
+  #After connection is established begin looking for flags
+  while True:
+    if (GPIO.input(sensor) == False):
+      print ("Flag is Up")
+      flag-down = False
+      client.publish('tipup', 'flag')
+      time.sleep(10)
+      
+    if (GPIO.input(sensor) == True):
+      print ("Flag is Down")
+      if (flag-down == False):
+        client.oublish('tipup', 'off')
+        flag-down = True;
+
+    if (GPIO.input(stealthSwitch) == False):
+      if (stealth == False):
+        print ("Silencing Beeper")
+        client.publish('tipup', 'stealth')
+        stealth = True
+      
+    if (GPIO.input(stealthSwitch) == True):
+      if (stealth == True):
+        stealth = False
+      
+      
 
 client = paho.Client()
+client.on_connect = on_connect
+
 client.connect("172.20.0.1", 1883)
-
-print ("CONNECTED")
-while True:
-  if (GPIO.input("XIO-P0") == False):
-    print ("IR Beam Broken")
-    time.sleep(1)
-    client.publish('tipup', 'flag')
-
-  if (GPIO.input("XIO-P2") == False):
-    print ("And this one does too motherfucker")
-    time.sleep(1)
-    client.publish('tipup', 'off')
+client.loop_forever()
 
 
-#def on_publish(mosq, obj, mid):
- # print ("JUST PUBLISHED")
 
-def on_disconnect(client, userdata, rc):
+
+
